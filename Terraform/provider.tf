26231b5a980573243c3d9b45da348a78be37f99b
jehad215 <gehadmosa51@gmail.com>
2023-02-14 03:07:43 +0200

    deleted

--- Terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = "jehad-iti"
-  region = "us-central1"
-  credentials = "./jehad-iti-8e0ca36af3f0.json"
-}
--- Terraform/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_network" "my-vpc" {
-  name = "my-vpc"
-  project = "jehad-iti"
-  auto_create_subnetworks = false
-  routing_mode = "REGIONAL"
-}
