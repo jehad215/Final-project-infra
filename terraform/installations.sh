7cf073bf8122c518b67791fdf4e885da74f9a3a4
jehad215 <gehadmosa51@gmail.com>
2023-02-15 01:52:18 +0200

    script

--- terraform/installations.sh
+++ terraform/installations.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #To install Google Cli
+
 sudo apt install -y apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
